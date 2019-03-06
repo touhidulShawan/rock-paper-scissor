d1fb1042028b3c9d838d95d50b7455a0a10a8336
Touhidul Shawan <touhidulislamshaon@gmail.com>
2019-03-06 20:05:06 +0600

    CHanges the game Ui

--- src/components/UserControl.js
+++ src/components/UserControl.js
@@ -1,20 +1,48 @@
 import React, { Component } from "react";
 import "../assets/css/UserControl.min.css";
-
 class UserControl extends Component {
+  state = {
+    playerChoice: null,
+    computerChoice: null
+  };
+
+  makeMove = event => {
+    const playerChoice = parseInt(
+      event.target.getAttribute("game-option-index")
+    );
+
+    const computerChoice = Math.floor(Math.random() * 3 + 1);
+
+    this.setState({ playerChoice, computerChoice });
+  };
+
   render() {
+    console.log(`Computer choice: ${this.state.computerChoice}`);
+    console.log(`Player choice: ${this.state.playerChoice}`);
+
     return (
       <div className="UserControl">
-        <div className="d-flex game-options">
-          <i className="fas fa-hand-rock" />
+        <div
+          className="d-flex game-options"
+          onClick={this.makeMove}
+          game-option-index="1"
+        >
+          <i className="fas fa-hand-rock" game-option-index="1" />
         </div>
-        <div className="d-flex game-options">
-          <i className="fas fa-hand-paper" />
+        <div
+          className="d-flex game-options"
+          onClick={this.makeMove}
+          game-option-index="2"
+        >
+          <i className="fas fa-hand-paper" game-option-index="2" />
         </div>
-        <div className="d-flex game-options">
-          <i className="fas fa-hand-scissors" />
+        <div
+          className="d-flex game-options"
+          onClick={this.makeMove}
+          game-option-index="3"
+        >
+          <i className="fas fa-hand-scissors" game-option-index="3" />
         </div>
-        <h5>Rock Paper Scissor</h5>
       </div>
     );
   }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,27 +1,19 @@
 import React from "react";
 import "../assets/css/App.min.css";
 import UserControl from "../components/UserControl";
-import PlayerChoice from "../components/PlayerChoice";
-import Winner from "../components/Winner";
 
 const App = () => {
   return (
-    <React.Fragment>
+    <div className="RPS">
       <div className="App">
         <h1>Rock Paper Scissor By React</h1>
       </div>
       <div className="row">
-        <div className="col-md-3">
+        <div className="col">
           <UserControl />
         </div>
-        <div className="col-md-4">
-          <PlayerChoice />
-        </div>
-        <div className="col-md-5">
-          <Winner />
-        </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
