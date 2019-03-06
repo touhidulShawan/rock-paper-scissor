564c6ff24a94c438bbed98d2d585b51f14f94f01
Touhidul Shawan <touhidulislamshaon@gmail.com>
2019-03-06 21:17:25 +0600

    Updates

---
+++ precache-manifest.b0c5b068bfbf21703bdac378ff20e790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce157157e6d9f37dbfb9",
+    "url": "/rock-paper-scissor/static/js/runtime~main.ce157157.js"
+  },
+  {
+    "revision": "56806952b9c557e4e8dd",
+    "url": "/rock-paper-scissor/static/js/main.56806952.chunk.js"
+  },
+  {
+    "revision": "3d920c09800488c0b050",
+    "url": "/rock-paper-scissor/static/js/2.3d920c09.chunk.js"
+  },
+  {
+    "revision": "56806952b9c557e4e8dd",
+    "url": "/rock-paper-scissor/static/css/main.85772100.chunk.css"
+  },
+  {
+    "revision": "3d920c09800488c0b050",
+    "url": "/rock-paper-scissor/static/css/2.85d0f250.chunk.css"
+  },
+  {
+    "revision": "b993ec21e2d669aad85f7da325f5aeee",
+    "url": "/rock-paper-scissor/index.html"
+  }
+];
